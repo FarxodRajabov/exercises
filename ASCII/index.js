9d0c192412de4261c3ea616281196851f79cbd89
Farxod <rajabovfarxod2@gmail.com>
2022-04-20 14:55:10 +0500

    new exercise

--- ASCII/index.js
+++ ASCII/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
+const input = data
 
-const [a, b] = data.split(' ').map(item => parseInt(item));
-fs.writeFileSync('output.txt', `${a+b}`, 'utf8');
+const res = input.charCodeAt(0)
+
+fs.writeFileSync('output.txt', `${res}`, 'utf8');
 
