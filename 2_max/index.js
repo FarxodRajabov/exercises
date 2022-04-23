cc6b93ba60004f309eb8ae42276f3069e8828d56
Farxod <rajabovfarxod2@gmail.com>
2022-04-23 16:05:52 +0500

    7%

--- 2_max/index.js
+++ 2_max/index.js
@@ -6,20 +6,15 @@ let numbers = nums.split(' ').map(elem => parseInt(elem))
 
 
 
-function arr(numbers) {
-    let max1 = 0
-    let max2 = numbers[1]
 
-    for (let i = 0; i < len; i++) {
-        if (numbers[i] > max1 ) {
-            max2 = max1
-            max1 = numbers[i]
-        } else if (numbers[i] > max2) {
-            max2 = numbers[i]
-        }
-    }
-    return max2
-}
+
+
+
+
+
+
+
+
 
 
 
