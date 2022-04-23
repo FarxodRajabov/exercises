75509fa91db9d45776cca0c062d4e46442e250bc
Farxod <rajabovfarxod2@gmail.com>
2022-04-23 16:06:46 +0500

    7%

--- 2_max/index.js
+++ 2_max/index.js
@@ -4,18 +4,20 @@ const data = fs.readFileSync('input.txt', 'utf8');
 const [len, nums] = data.split('\n');
 let numbers = nums.split(' ').map(elem => parseInt(elem))
 
+let max1 = 0
+let max2 = 0 
 
-
-
-
-
-
-
-
-
-
-
-
+function arr(numbers) {
+    for(let i = 0; i < len; i++) {
+      if(numbers[i] > max1) {
+        max2 = max1
+        max1 = numbers[i]
+      }else if (numbers[i] > max2) {
+          max2 = numbers[i]
+      }
+    }
+    return max2
+}
 
 
 fs.writeFileSync('output.txt', `${arr(numbers)}`, 'utf8');
