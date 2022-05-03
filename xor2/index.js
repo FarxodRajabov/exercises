e9c4b46f704d07fe028b2e577b57202747adf7bf
Farxod <rajabovfarxod2@gmail.com>
2022-05-03 22:44:36 +0500

    7%

---
+++ xor2/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf8');
+
+const [a,...arr] = data.split('\n')
+
+
+const [b,c] = arr.map(elem => elem.split(' '))
+
+
+function xor(a,b,c) {
+    let array = []
+    for (let i = 0; i < +a; i++) {
+        array.push(Math.abs(b[i] - c[i]))           
+    } 
+
+    return array.join(' ')
+}
+
+fs.writeFileSync('output.txt', `${xor(a,b,c)}`, 'utf8');
+
