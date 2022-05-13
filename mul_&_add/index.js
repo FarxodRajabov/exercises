e3754d83d2fe18ecceedf850d71f64a2cb4ed300
Farxod <rajabovfarxod2@gmail.com>
2022-05-13 14:21:46 +0500

    dont solved

---
+++ mul_&_add/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf8');
+
+const n = data.split('\r')
+
+const [a,b] = n
+
+function res(a,b) {
+    if(b === undefined) {
+        return a.split(' ').reduce((sum,elem) => (sum + +elem), 0)
+    }else {
+        return a * b
+    }
+}
+
+
+
+fs.writeFileSync('output.txt', `${res(a,b)}`, 'utf8');
+
