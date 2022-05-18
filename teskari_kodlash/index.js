8f4cc3bc7d7d2adda015deeb88f35e37d5642869
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-05-18 22:52:57 +0500

    bug fixed

--- teskari_kodlash/index.js
+++ teskari_kodlash/index.js
@@ -1,20 +1,5 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 
-const [a, ...b] = data.split('\n');
-const [c,d,e,f] = b.map(elem => elem.split(' '))
-
-const sum = ''
-function res(c,d,e,f) {
-    if(+c[1] % 2 === 0) {
-        
-    }
-}
-
-
-console.log(c,d,e,f);
-
-
-
 fs.writeFileSync('output.txt', `${a+b}`, 'utf8');
 
