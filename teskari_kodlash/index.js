17d134eab519f4d3eefc05b4a310b449312311d2
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-05-18 23:26:18 +0500

    was not solved

--- teskari_kodlash/index.js
+++ teskari_kodlash/index.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 
-fs.writeFileSync('output.txt', `${a+b}`, 'utf8');
+const [testsCount, ...pairs] = data.split('\n')
 
+const numbers = pairs.map(pair => pair.split(' ').map(elem => parseInt(elem)))
+
+const result = numbers.reduce((res, elem) => {
+    if (elem[0] % 2 === 0) {
+        res += elem[0] / 2 * (elem[0] + 1) === elem[1] ? '1' : '0'
+    } else {
+        res += Math.ceil(elem[0] / 2) * elem[0] === elem[1] ? '1' : '0'
+    }
+
+    return res
+}, '')
+
+
+fs.writeFileSync('output.txt', `${result}`, 'utf8');
