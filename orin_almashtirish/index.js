84215137a17ce31350530fddefc69b58b2308c21
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-05-24 19:52:49 +0500

    demak ishlaykan

---
+++ orin_almashtirish/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf8');
+
+let [a, b] = data.split(' ').map(item => parseInt(item));
+
+console.log(`${a} ${b}`);
+
+a = a + b
+b = a - b
+a = a - b
+
+console.log(`${a} ${b}`);
+
+
+fs.writeFileSync('output.txt', `${b} ${a}`, 'utf8');
+
