cdb12c8b8c4340dace6191566988f2b29aae89d9
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-06-10 14:52:26 +0500

    changed

--- tub_son/index.js
+++ tub_son/index.js
@@ -3,8 +3,16 @@ const n = fs.readFileSync('input.txt', 'utf8');
 
 function tub(n) {
     let array = []
+    let sqrti = 1
+    let kvi = 1
+    
     for (let i = 2; i <= +n; i++) {
-        for (let a = 2; a < Math.sqrt(i); a++) {
+        if(kvi < i) {
+            sqrti += 1
+            kvi = sqrti*sqrti
+        }
+
+        for (let a = 2; a < sqrti; a++) {
             if (i % a === 0) {
                 break
             }else {
