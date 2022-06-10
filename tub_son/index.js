0fa4c5cf9046f1a5c38857c89e5db02ce2076cb5
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-06-10 15:20:28 +0500

    solved by Oybek

--- tub_son/index.js
+++ tub_son/index.js
@@ -1,28 +1,36 @@
 const fs = require('fs');
 const n = fs.readFileSync('input.txt', 'utf8');
 
+
 function tub(n) {
     let array = []
     let sqrti = 1
     let kvi = 1
+    let isPrime = 0
     
     for (let i = 2; i <= +n; i++) {
+        isPrime = true
         if(kvi < i) {
             sqrti += 1
             kvi = sqrti*sqrti
         }
 
-        for (let a = 2; a < sqrti; a++) {
+        for (let a = 2; a <= sqrti; a++) {
             if (i % a === 0) {
-                break
-            }else {
-                array.push(i)
+                isPrime =false
                 break
             }
+        }
+        if(isPrime) {
+            array.push(i)
         }
     }
     return array
 }
 
 fs.writeFileSync('output.txt', `${tub(n)}`, 'utf8');
+
+
+
+
 
