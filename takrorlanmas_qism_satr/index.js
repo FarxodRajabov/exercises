2d4989e377216548cbba6fbaebd59c52b406047e
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-06-11 19:52:39 +0500

    10%

---
+++ takrorlanmas_qism_satr/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf8');
+
+const [a, ...b] = data.split('\n')
+const c = +a 
+
+
+let isExist = 0
+
+
+function str(a, b) {
+    let res = ''
+    const elems = b.map(elem => elem.split(''))
+    for (let i = 0; i < a; i++) {
+        res = res + `${elems[i].length - new Set(elems[i]).size}\n`
+    }
+    return res
+}
+
+
+fs.writeFileSync('output.txt', `${str(c,b)}`, 'utf8');
+
