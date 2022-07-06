5d898fb31d2a19bbbca09e2966b559c61b2fb3ed
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-07-06 15:28:44 +0500

    13%

--- pdf/index.js
+++ pdf/index.js
@@ -1,6 +1,24 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 
-const [a, b] = data.split(' ').map(item => parseInt(item));
-fs.writeFileSync('output.txt', `${a+b}`, 'utf8');
+const input = data.split('\n')
 
+const [a,c] = input.map(elem => elem.split(' '))
+
+const nums = a.map(elem => +elem)
+
+const [str]= c
+
+function pdf(a,c) {
+    let arr = []
+    let maxHeight = 0
+    let res = 0
+    for (let i = 0; i < str.length; i++) {
+        arr.push(nums[str[i].charCodeAt() - 97])
+    }
+    maxHeight = Math.max(...arr)
+    res = maxHeight * str.length
+    return res
+}
+
+fs.writeFileSync('output.txt', `${pdf(a,c)}`, 'utf8');
