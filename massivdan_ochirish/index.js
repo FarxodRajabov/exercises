f7313a482881425ba6d77e4f70cce8957f6ac5b0
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-07-06 13:23:50 +0500

    arr2

--- massivdan_ochirish/index.js
+++ massivdan_ochirish/index.js
@@ -2,23 +2,29 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 
 const [a, b] = data.split('\n')
-lenArr= +a
+lenArr = +a
 const Arr = b.split(' ')
 
-function arr(a,b) {
+let maxCount = 1
+
+function arr(lenArr,b) {
     const map = new Map()
     let res = 0
     let getMap = 0
+    let MapNumsCount = 0
     for (let i = 0; i < lenArr; i++) {
         getMap = map.get(b[i])
         if (!getMap){
             map.set(Arr[i],1)
         }else{
-            map.set(Arr[i], getMap + 1)
+            MapNumsCount = getMap + 1
+            map.set(Arr[i], MapNumsCount)
         }
     }
     let val = [...map.values()]
     res = lenArr - Math.max(...val)
     return res
 }
+
+
 fs.writeFileSync('output.txt', `${arr(lenArr,Arr)}`, 'utf8');
